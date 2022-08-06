5d7f125f5c64a877c908458347493d0e6ce38c22
Jp <jyotiprakashpc@outlook.com>
2022-08-06 23:20:32 +0530

    Code to edit contact details

---
+++ AddressBookMainUC3.cs
@@ -0,0 +1,66 @@
+﻿namespace AddressBookSystem
+{
+    public class AddressBookMainUC3
+    {
+        static char choice = 'y';
+        static int index = 0;
+        static int option;
+        static List<EditContactUC3> contacts = new List<EditContactUC3>();
+        public static void Main()
+        {
+            Console.WriteLine("Welcome To Address Book Program.");
+            Option();
+        }
+        public static void Option()
+        {
+            Console.Write("1. Add Contact \n2. Edit Contact \nSelect your choice = ");
+            option = Convert.ToInt32(Console.ReadLine());
+            ExecuteOption();
+        }
+        public static void ExecuteOption()
+        {
+            if (option == 1)
+            {
+                while (choice == 'y' || choice == 'Y')
+                {
+                    contacts.Add(new EditContactUC3());
+                    contacts[index].GetDetail();
+
+                    Console.Write("Do you want to add more press 'y' or Press any key to Exit = ");
+                    choice = Convert.ToChar(Console.ReadLine());
+                    ++index;
+                }
+            }
+            else if (option == 2)
+            {
+                Console.Write("Enter First Name to find Contact & Edit = ");
+                string search_Name = Console.ReadLine();
+                int i;
+                for (i = 0; i < index; i++)
+                {
+                    int find = contacts[i].FindContact(search_Name);
+                    if (find == 0)
+                        continue;
+                    else
+                    {
+                        contacts[i].ViewDetail();
+                        break;
+                    }
+                }
+                if (i != index)
+                    contacts[i].EdiDetail();
+                else
+                {
+                    Console.WriteLine("No Records Found.");
+                    Option();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid option");
+                Option();
+            }
+        }
+    }
+}
---
+++ EditContactUC3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class EditContactUC3
+    {
+        private string first_Name;
+        private string last_Name;
+        private string state;
+        private string city;
+        private int pincode;
+        private long phone_Nmuber;
+        private string email;
+
+        public void GetDetail()
+        {
+            Console.Write("Enter First Name = ");
+            first_Name = Console.ReadLine();
+            Console.Write("Enter Last Name = ");
+            last_Name = Console.ReadLine();
+            Console.Write("Enter State Name = ");
+            state = Console.ReadLine();
+            Console.Write("Enter City Name = ");
+            city = Console.ReadLine();
+            Console.Write("Enter Pincode = ");
+            pincode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Phone Number = ");
+            phone_Nmuber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter E-Mail = ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Contact Created. Details Saved.");
+        }
+        public void ViewDetail()
+        {
+            Console.Write("First Name = " + first_Name);
+            Console.Write("Last Name = " + last_Name);
+            Console.Write("State = " + state);
+            Console.Write("City = " + city);
+            Console.Write("Pincode = " + pincode);
+            Console.Write("Phone Number = " + phone_Nmuber);
+            Console.Write("E-Mail = " + email);
+        }
+        public void EdiDetail()
+        {
+            Console.Write("Enter First Name = ");
+            first_Name = Console.ReadLine();
+            Console.Write("Enter Last Name = ");
+            last_Name = Console.ReadLine();
+            Console.Write("Enter State Name = ");
+            state = Console.ReadLine();
+            Console.Write("Enter City Name = ");
+            city = Console.ReadLine();
+            Console.Write("Enter Pincode = ");
+            pincode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Phone Number = ");
+            phone_Nmuber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter E-Mail = ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Edit Saved.");
+        }
+        public int FindContact(string search_Name)
+        {
+            if (first_Name.Equals(search_Name))
+                return 1;
+            else
+                return 0;
+        }
+    }
+}
