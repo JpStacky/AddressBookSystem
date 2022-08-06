05a34d2b7f28c6cded565826693629df3ab3b182
Jp <jyotiprakashpc@outlook.com>
2022-08-06 23:18:58 +0530

    Create New Contact in RunTime

---
+++ AddressBookMainUC2.cs
@@ -0,0 +1,23 @@
+﻿namespace AddressBookSystem
+{
+    public class AddressBookMainUC2
+    {
+        static char choice = 'y';
+        static int index = 0;
+        public static void Main()
+        {
+            Console.WriteLine("Welcome To Address Book Program.");
+            List<ContactUC1> contacts = new List<ContactUC1>();
+            //Calling method to take input
+            while (choice == 'y' || choice == 'Y')
+            {
+                contacts.Add(new ContactUC1());
+                contacts[index].GetDetail();
+
+                Console.Write("Do you want to add more press 'y' or Press any key to Exit = ");
+                choice = Convert.ToChar(Console.ReadLine());
+                ++index;
+            }
+        }
+    }
+}
