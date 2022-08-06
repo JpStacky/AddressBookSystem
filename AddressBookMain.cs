573f344b4c836f649e999b888fd936449a04ace9
Jp <jyotiprakashpc@outlook.com>
2022-08-06 23:13:30 +0530

    Code to call method to take user input data

--- AddressBookMain.cs
+++ AddressBookMain.cs
@@ -5,6 +5,10 @@
         public static void Main()
         {
             Console.WriteLine("Welcome To Address Book Program.");
+
+            //Calling method to take input
+            ContactUC1 contact = new ContactUC1();
+            contact.GetDetail();
         }
     }
 }
---
+++ ContactUC1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class ContactUC1
+    {
+        private string first_Name;
+        private string last_Name;
+        private string state;
+        private string city;
+        private int pincode;
+        private long phone_Nmuber;
+        private string email;
+
+        public void GetDetail()
+        {
+            Console.Write("Enter First Name = ");
+            first_Name = Console.ReadLine();
+            Console.Write("Enter Last Name = ");
+            last_Name = Console.ReadLine();
+            Console.Write("Enter State Name = ");
+            state = Console.ReadLine();
+            Console.Write("Enter City Name = ");
+            city = Console.ReadLine();
+            Console.Write("Enter Pincode = ");
+            pincode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Phone Number = ");
+            phone_Nmuber = Convert.ToInt64(Console.ReadLine());
+            Console.Write("Enter E-Mail = ");
+            email = Console.ReadLine();
+
+            Console.WriteLine("Contact Created. Details Saved.");
+        }
+    }
+}
