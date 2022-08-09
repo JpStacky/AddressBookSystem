bfaac9da542633df28051e6cff8d5ad57fa4da0b
JpStacky <62001159+JpStacky@users.noreply.github.com>
2022-08-09 09:16:43 +0530

    Update AddressBookMainUC2.cs

--- AddressBookMainUC2.cs
+++ AddressBookMainUC2.cs
@@ -2,21 +2,12 @@
 {
     public class AddressBookMainUC2
     {
-        static char choice = 'y';
-        static int index = 0;
         public static void Main()
         {
             Console.WriteLine("Welcome To Address Book Program.");
-            List<ContactUC1> contacts = new List<ContactUC1>();
-            //Calling method to take input
-            while (choice == 'y' || choice == 'Y')
-            {
-                contacts.Add(new ContactUC1());
-                contacts[index].GetDetail();
-
-                Console.Write("Do you want to add more press 'y' or Press any key to Exit = ");
-                choice = Convert.ToChar(Console.ReadLine());
-                ++index;
+            
+             ContactUC1 contact = new ContactUC1();
+              Contact.GetDetail();
             }
         }
     }
