2dde3ce724e05a1ed26a065b2da5c86bf5231a5c
Jp <jyotiprakashpc@outlook.com>
2022-08-09 11:57:23 +0530

    Multiple Adress Book

---
+++ AddressBookMainUC6.cs
@@ -0,0 +1,14 @@
+﻿namespace AddressBookSystem
+{
+    public class AddressBookMainUC6
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome To Address Book Program.");
+            MultipleAddressBookUC6 addressbook1 = new MultipleAddressBookUC6();
+            MultipleAddressBookUC6 addressbook2 = new MultipleAddressBookUC6();
+            addressbook1.CreateContactBook();
+            addressbook2.CreateContactBook();
+        }
+    }
+}
---
+++ MultipleAddressBookUC6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class MultipleAddressBookUC6
+    {
+        static char choice = 'y';
+        static int index = 0;
+        List<ContactBookUC6> contacts = new List<ContactBookUC6>();
+        public void CreateContactBook()
+        {
+            while (choice == 'y' || choice == 'Y')
+            {
+                contacts.Add(new ContactBookUC6());
+                contacts[index].GetDetail();
+
+                Console.Write("Do you want to add more press 'y' or Press any key to Exit = ");
+                choice = Convert.ToChar(Console.ReadLine());
+                ++index;
+            }
+        }
+    }
+}
